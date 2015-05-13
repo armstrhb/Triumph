d09d440a907af080aef894e8712780d7315a3457
armstrhb <henry.b.armstrong@gmail.com>
2015-05-12 22:37:53 -0400

    unit tests for realm/ach

--- test/models/achievement_test.rb
+++ test/models/achievement_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class AchievementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "list achievements" do
+    assert Achievement.all.length > 0, "achievements list empty"
+  end
+
+  test "test individual achievements" do
+    Achievement.all.each { |a|
+      assert_not a == nil, "first achievement is null"
+      assert_not a.title.empty?, "title is empty"
+      assert_not a.description.empty?, "description is empty"
+      assert_not a.realm == nil, "realm is null"
+      assert_not a.category == nil, "category is null"
+      assert_not a.rarity == nil, "rarity is null"
+      assert a.points >= 0, "points value is negative"
+      assert a.required_ticks >= 0, "required ticks value is negative"
+    }
+  end
 end
--- test/models/realm_test.rb
+++ test/models/realm_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class RealmTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "realm initial test" do
+    Realm.all.each { |r|
+      assert_not r == nil, "realm is nil"
+      assert_not r.name.empty?, "realm name is empty"
+      assert_not r.group == nil, "admin group is nil"
+    }
+  end
+
+  test "realm save within name" do
+    realm = Realm.new
+    assert_not realm.save, "saved realm without a name"
+  end
+
+  test "realm create" do
+    realm = Realm.new
+    realm.name = "test-realm"
+    realm.active = false
+
+    group = Group.new
+    group.name = "test-realm-admins"
+
+    realm.group = group
+    assert realm.save, "save realm failed"
+  end
+
+  test "realm change group" do
+    realm = Realm.new
+    realm.name = "test-realm2"
+    realm.active = false
+
+    group = Group.new
+    group.name = "test-realm2-admins"
+
+    realm.group = group
+    realm.save
+
+    group1 = Group.new
+    group1.name = "the-replacement-group"
+
+    realm.group = group1
+    realm.save
+
+    realm_check = Realm.find_by(name: realm.name)
+    assert realm_check.group.name == realm.group.name, "group change didn't commit"
+  end
 end
