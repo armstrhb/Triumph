162413ded36088ba50c3cd6fad82264046a42f42
armstrhb <henry.b.armstrong@gmail.com>
2015-05-21 21:46:17 -0400

    User name unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :realms, :join_table => "realm_users"
   has_many :progresses;
 
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :active, inclusion: { in: [true, false] }
 end
