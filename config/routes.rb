76c65b1a93b52316547bde328a9512b8dbd3cca4
armstrhb <henry.b.armstrong@gmail.com>
2015-05-22 23:17:31 -0400

    User login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/index'
 
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :realms
   resources :groups
