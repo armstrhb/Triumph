cf9b5afcf64023c65616d6d473009ac8de8898b5
armstrhb <henry.b.armstrong@gmail.com>
2015-05-25 20:37:07 -0400

    redirect>forward for within session

--- app/controllers/realms_controller.rb
+++ app/controllers/realms_controller.rb
@@ -95,6 +95,7 @@ class RealmsController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid username/password combination'
       render 'new'
