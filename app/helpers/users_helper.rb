7aa706b53beb43b17e985deb548a8de3140e6f11
armstrhb <henry.b.armstrong@gmail.com>
2015-05-27 22:24:25 -0400

    Realm detail user includes gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
-  def gravatar_for(user, size=40)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  def gravatar_for(email, size=40)
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=retro&s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+
+    image_tag(gravatar_url, class: "gravatar")
   end
 end
