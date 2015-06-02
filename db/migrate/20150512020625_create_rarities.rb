c09c93bcf6b18555826da7445ef256c64751cb8a
armstrhb <henry.b.armstrong@gmail.com>
2015-06-02 14:36:19 -0400

    Rarity Description

--- db/migrate/20150512020625_create_rarities.rb
+++ db/migrate/20150512020625_create_rarities.rb
@@ -4,6 +4,7 @@ class CreateRarities < ActiveRecord::Migration
       t.string :name, null: false
       t.index :name
       t.belongs_to :realm
+      t.string :description, null: false
       t.timestamps null: false
     end
   end
